75fbced6761f1d24676ab5b7474c4af9aa6c4a69
Pak Ming Lau <pak.plau@gmail.com>
2021-04-25 18:16:08 -0400

    Add rendering of film titles to the webpage

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,28 @@
-from flask import Flask, render_template
-import os
+from flask import Flask, render_template, request
+import urllib, json
+
+f = open("keys/tmdb.txt", "r")
+tmdb_link = "https://api.themoviedb.org/3/discover/movie?api_key=" + f.read() + "&with_genres="
+f.close()
 
 app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def root():
-    return render_template("home.html")
+    return render_template("home.html", movies="")
 
+@app.route("/get_movies", methods=["GET", "POST"])
+def get_movies():
+    movies = request.form.getlist("genre")
+    if not movies:
+        return render_template("home.html", error="Pick at least one genre!", movies="")
+    else:
+        temp = tmdb_link
+        for i in movies:
+            temp += str(i) + ","
+        temp = temp[:len(temp) - 1]
+        print(temp)
+        return render_template("home.html", movies=json.loads(urllib.request.urlopen(temp).read()))
 
 if(__name__ == "__main__"):
-    app.debug = False
+    app.debug = True
     app.run()

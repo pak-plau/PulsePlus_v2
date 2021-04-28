45ac0a4be46a0eb88052aa7bc21d357b8ac7da2a
Victoria Gao <71650633+vgao1@users.noreply.github.com>
2021-04-27 20:32:16 -0400

    get movies from search results

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,9 @@ from flask import Flask, render_template, request
 import urllib, json
 
 f = open("keys/tmdb.txt", "r")
-tmdb_link = "https://api.themoviedb.org/3/discover/movie?api_key=" + f.read() + "&with_genres="
+key = f.read()
+tmdb_link = "https://api.themoviedb.org/3/discover/movie?api_key=" + key + "&with_genres="
+title_link = "https://api.themoviedb.org/3/search/movie?api_key=" + key+"&include_adult=true&query="
 f.close()
 
 app = Flask(__name__)
@@ -22,6 +24,12 @@ def get_movies():
         temp = temp[:len(temp) - 1]
         print(temp)
         return render_template("home.html", movies=json.loads(urllib.request.urlopen(temp).read()))
+    
+@app.route("/find_title", methods=["GET", "POST"])
+def find_title():
+    title = request.form.get("search").replace(" ","+")
+    temp = title_link+title
+    return render_template("home.html", movies=json.loads(urllib.request.urlopen(temp).read()))
 
 if(__name__ == "__main__"):
     app.debug = True
